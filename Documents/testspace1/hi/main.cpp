ce90b51e8815d1cacf4a76dd780f126f2e3a630e
aserebrii <lakeasandr@yahoo.com>
2018-02-21 20:39:19 -0500

    Adding main.cpp

---
+++ Documents/testspace1/hi/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+	vector <string> v;
+	string h = "See this wasnt that hard";
+	v.push_back("ok");
+	v.push_back("Ok");
+	v.push_back("OK");
+	v.push_back("*queue earrape*");
+	v.push_back("*queue earrape*");
+	v.push_back("*queue earrape*");
+	v.push_back("*queue earrape*");
+	v.push_back("*queue earrape*");
+	v.push_back("*queue earrape*");
+	v.push_back("*queue earrape*");
+	for(int i = 0;i<10;i++)
+	{
+		cout<<"nice"<<endl;
+		cout<<v[i]<<endl;
+		
+	}
+	cout<<h;
+	return 0;
+}
