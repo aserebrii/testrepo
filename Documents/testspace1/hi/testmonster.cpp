38c88e96ee97e3d1d8ffecf8c4c9e6fe5c926dac
aserebrii <lakeasandr@yahoo.com>
2018-02-22 16:35:51 -0500

    added some implentation to monsters

---
+++ Documents/testspace1/hi/testmonster.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <time.h>
+#include <stdlib.h>
+#include "atk.h"
+#include "testmonster.h"
+
+using namespace std;
+
+monst::monst()
+{
+	
+}
+monst::monst(string infostring)
+{
+	
+}
+int monst::getAC()
+{
+	return ac;
+}
